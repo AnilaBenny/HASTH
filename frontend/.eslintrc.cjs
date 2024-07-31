906da3aa40e85eb1cecaaead2270b8d95cf9814d
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-07-31 17:55:47 +0530

    changed to react app

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'react-refresh',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
