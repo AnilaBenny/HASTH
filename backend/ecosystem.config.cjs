bfb79a919df8f0ea02d3bb2c4ad31dd3e8bdaba8
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-09-21 15:23:39 +0530

    passport updated

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "server",                     // Name of the application
-      script: "dist/app.js",              // Path to the compiled JavaScript file
-      instances: 1,                       // Number of instances (set to 1 for single instance or 0 for max CPUs)
-      exec_mode: "cluster",               // Cluster mode for scaling across multiple cores
-      autorestart: true,                  // Automatically restart the app on crash or failure
-      watch: false,                       // No need to watch files in production
-      max_memory_restart: '1G',           // Restart the app if it exceeds 1GB memory usage
+      name: "server",                   
+      script: "dist/app.js",              
+      instances: 1,                       
+      exec_mode: "cluster",               
+      autorestart: true,                  
+      watch: false,                       
+      max_memory_restart: '1G',           
 
     },
   ],
