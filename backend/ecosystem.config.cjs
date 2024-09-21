46e07e2cf03f11969d5f2174b624861c99128541
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-09-21 13:09:00 +0530

    ecosystem updated

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,16 +1,13 @@
-
 module.exports = {
-    apps: [
-      {
-        name: 'server',
-        script: './server.ts',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
-        interpreter: 'ts-node',
-        interpreter_args: '-r ts-node/register',
-      },
-    ],
-  };
-  
+  apps: [
+    {
+      name: 'server',
+      script: 'src/server.ts',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      interpreter: 'ts-node',
+    },
+  ],
+};
