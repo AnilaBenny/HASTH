38fa6cf8a9bc0c17e3fc19b7528de6ce90d40338
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-09-24 16:24:17 +0530

    search useCase bug fixed

--- backend/src/application/useCase/userUseCase/searchUseCase.ts
+++ backend/src/application/useCase/userUseCase/searchUseCase.ts
@@ -6,13 +6,9 @@ export default(dependencies: any)=> {
     const executeFunction = async (data:any) => {
         try {
             const response = await userRespository.search(data);
-            console.log(response);
-            
-            if (response.status) {
-                return { status: true, data: response.data };
-            } else {
-                return { status: false, message: response.message || "search failed" };
-            }
+
+                return { status: true, data: response };
+
         } catch (error) {
             logger.error("Error in search usecase:", error);
             return { status: false, message: "Error in search use case" };
