b9e5214a2d9ebfb450eddfcf239cb75bd7d6c038
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-09-29 23:40:03 +0530

    typeerror removed

--- frontend/src/components/Footer/FooterBanner.tsx
+++ frontend/src/components/Footer/FooterBanner.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const FooterBanner = () => {
+const FooterBanner:React.FC= () => {
   const [email, setEmail] = useState('');
 
   const handleSubscribe = () => {
