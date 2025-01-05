ce6fa36ef07c4c61d8cbe0a9c5e6b7893c57a74a
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2025-01-06 01:15:19 +0530

    url updated

--- frontend/src/Axiosconfig/Axiosconfig.tsx
+++ frontend/src/Axiosconfig/Axiosconfig.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://hasth.onrender.com",
+  baseURL: "https://hasth.mooo.com",
   timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
